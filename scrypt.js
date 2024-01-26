6f0324f37b272f2f4440a997a96eb56336828259
AKonevka <122662320+AKonevka@users.noreply.github.com>
2024-01-26 16:37:22 +0500

    Add files via upload

--- scrypt.js
+++ scrypt.js
@@ -2,16 +2,36 @@ const myButton = document.querySelector('#myButton');
 const nameInput = document.querySelector('#nameInput');
 const list = document.querySelector('#list');
 
+let num = 1;
+let num2 = 0;
 
 myButton.addEventListener('click', (event) => {
     const newItem = document.createElement('li');
+
     // element.classList — Добавляет или удаляет классы для элемента
     //newItem.setAttribute('id', 'newItem');
+    //newCommentImg.id = 'id';
     newItem.classList.add('item');
     newItem.textContent = nameInput.value;
+    //let item = document.getElementsByClassName('#item');
     // Добавить кнопку для удаления
+    //const objectButton = document.createElement('div')
     const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Удалить';
+    deleteButton.classList.add('btn');
+    //deleteButton.textContent = 'Удалить';
+
+
+    if (num == 1) {
+        num = 2;
+        newItem.style = `background: rgb(249, 249, 249);`
+        deleteButton.style = `background: rgb(249, 249, 249);`
+    } else {
+        num = 1;
+        newItem.style = `background: rgb(238, 238, 238);`
+        deleteButton.style = `background: rgb(238, 238, 238);`
+    }
+    console.log(num);
+
 
     // Удаление элмента списка
     deleteButton.addEventListener('click', () => {
@@ -26,11 +46,13 @@ myButton.addEventListener('click', (event) => {
     list.appendChild(newItem);
     // очистка поля
     //nameInput.value = '';
+    newItem.addEventListener('click', function (event) {
+        newItem.style = `background: rgb(136, 136, 136);`
+        deleteButton.style = `background: rgb(136, 136, 136);`
+    })
 })
-//let newItem = document.getElementsByClassName('newItem');
-//newItem.addEventListener('keydown', function (event) {
 
-//})
+
 
 
 
